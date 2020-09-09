448d77ae81a2a1bb6501797c2efd168228748a98
Dummerle <lennardbaeumer@gmail.com>
2020-09-09 21:13:45 +0200

    Test

--- CashApp/API/app.py
+++ CashApp/API/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, jsonify, make_response, Response, request
+import _hashlib
+
+from flask import Flask, render_template, jsonify, make_response, Response, request, redirect
 
 app = Flask(__name__)
+adminkey = "d404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db"
+
+
+def get_hash(text: str):
+    return _hashlib.openssl_sha512(text.encode()).hexdigest()
 
 
 @app.route('/')
 def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/second')
+def second():
     return render_template("index.html")
 
 
@@ -20,15 +32,23 @@ def getData():
     return resp
 
 
-@app.route("/api/send", methods=["POST", "GET", "OPTIONS"])
+@app.route("/api/send", methods=["POST", "OPTIONS"])
 def send():
-    print("Hi")
-    print(request.method)
-    resp:Response = make_response()
-    resp.headers["Access-Control-Allow-Origin"] = "*"
-    resp.headers["Access-Control-Allow-Methods"] = "POST,GET"
-    resp.headers["Access-Control-Allow-Headers"] = "Content-Type, Access-Control-Allow-Headers"
+    resp: Response = make_response(jsonify(1), 200)
+    password = ""
+    if request.method == "POST":
+        data = request.get_data(as_text=True)
+        data = eval(data)
+        password = str(data['password'])
 
+    if get_hash(password)==adminkey:
+        resp.set_cookie("adminkey", adminkey)
+        print("cookie set")
+    resp.headers["Access-Control-Allow-Origin"] = "*"
+    # resp.headers["Access-Control-Allow-Methods"] = "POST,GET"
+    resp.headers["Access-Control-Allow-Headers"] = "Content-Type, Access-Control-Allow-Headers"
+    resp.headers["Access-Control-Allow-Credentials"] = "true"
+    resp.set_cookie("hallo", "Peter")
     return resp
 
 
--- CashApp/src/app/second/second.component.ts
+++ CashApp/src/app/second/second.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
+import {CookieService} from 'ngx-cookie-service';
+
 @Component({
     selector: 'app-second',
     templateUrl: './second.component.html',
@@ -9,24 +11,20 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class SecondComponent implements OnInit {
     test;
-    head = new HttpHeaders({
-        'Access-Control-Allow-Origin': '*'
-    });
-
-    options = {headers: this.head};
+    resp;
+    correctForm = true;
     apiGetUrl = 'http://localhost:5000/api/getData';
     apiPostUrl = 'http://localhost:5000/api/send';
     x: any;
     loginForm = new FormGroup({
-        name: new FormControl(null, [Validators.required]),
-        password: new FormControl()
+        password: new FormControl(null, Validators.required)
     });
 
-    constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+    constructor(private http: HttpClient, private cookie: CookieService) {
     }
 
     ngOnInit(): void {
-        /*
+
         this.http.get(this.apiGetUrl).subscribe(
             ok => {
                 this.x = ok;
@@ -37,13 +35,14 @@ export class SecondComponent implements OnInit {
             fil => {
             }
         );
-*/
+
     }
 
     onSubmit(): void {
         console.log(this.loginForm.value);
         if (this.loginForm.valid) {
-            this.http.post(this.apiPostUrl, this.loginForm.value).subscribe(
+            this.correctForm = true;
+            this.resp = this.http.post(this.apiPostUrl, this.loginForm.value).subscribe(
                 ok => {
                     console.log(ok);
                 },
@@ -52,8 +51,10 @@ export class SecondComponent implements OnInit {
                 }
             );
         } else {
-            console.log('Falsche Eingabe');
+            this.correctForm = false;
         }
     }
-
+    logCookie(): void{
+        console.log(this.cookie.getAll());
+    }
 }
